2517a3cd69f184c01f6d2ce8fc4dd4384b379690
garrett-yoon <ghjyoon@gmail.com>
2022-05-12 08:48:05 -0400

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,37 @@
-<script>
+<script type="text/javascript">
+	let factors = []
+	let risks = [
+		{id: 'Age > 65', val: 1, done: false},
+		{id: 'Age > 75', val: 1, done: false},
+		{id: 'Female Sex', val: 1, done: false},
+		{id: 'CHF Hx', val: 1, done: false},
+		{id: 'HTN Hx', val: 1, done: false},
+		{id: 'Prior Stroke', val: 2, done: false},
+		{id: 'Prior MI/PAD', val: 1, done: false},
+		{id: 'Diabetes Hx', val: 1, done: false},
+	]
+	let elements = 0
 	
+	$: selected = risks.filter(t => t.done)
+
+	$: sumS = selected.reduce((s,a) => s + a.val, 0);
+
 </script>
 
 <main>
-	<h1>Hello</h1>
-	<input type="checkbox"> Expansions <br>
+
+	<h1>CHA2DS2VASc</h1>
+	{#each risks as risk (risk)}
+	<label>
+		<input name="riskfactor" type="checkbox" bind:checked={risk.done}> 
+		{risk.id} 
+		<br>
+	</label>	
+	
+	{/each}
+	<p style="font-size:50px">CHA2DSVASc SCORE: {sumS}</p>
+
+
 </main>
 
 <style>
